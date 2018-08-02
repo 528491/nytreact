728754cafc1c123cebc77e378980650ce83d86f8
528491 <jacobminasian@gmail.com>
2018-08-01 22:47:00 -0700

    Default react app served via express server

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ var app = express();
 //3001 chosen so as not to conflict with the port the react server will be listening on
 var PORT = process.env.PORT || 3001;
 
+app.use(express.static(path.join(__dirname, 'nytreact-client/build')));
+
 //Routes will be defined here minimally, to get functionality up and running ASAP.
 //Routes can then be defined in a more modular fashion after main functionality (implemented in react) is 
 //implemented successfully
