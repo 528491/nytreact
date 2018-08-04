8afe1e1f207af7ebfbc8b3cafa0025fa5143c21e
528491 <jacobminasian@gmail.com>
2018-08-04 00:30:40 -0700

    Static route to react app and routes we defined for api no longer conflict with one another.

---
+++ routes/api/articles.js
@@ -0,0 +1,12 @@
+
+/*
+app.get("/api/articles", function(req, res){
+    Article.find({}, function(err, data){
+        if (err){
+            console.log(err);
+        }
+        console.log("We hit this route");
+        res.json(data);
+    });
+});
+*/
--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const mongoose = require('mongoose');
 const Article = require('./models/Article');
 const routes = require("./routes");
 
-
 //Will connect to mongoose in this single file as well - goal is to get an "mvp"
 //up and running before refactoring
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/nytreact";
@@ -15,35 +14,8 @@ const app = express();
 //3001 chosen so as not to conflict with the port the react server will be listening on
 const PORT = process.env.PORT || 3001;
 
-//app.use(express.static(path.join(__dirname, 'nytreact-client/build')));
-//app.use(express.static(path.join(__dirname, "")));
-//Routes will be defined here minimally, to get functionality up and running ASAP.
-//Routes can then be defined in a more modular fashion after main functionality (implemented in react) is 
-//implemented successfully
-
-/*
-app.get("/api/helloworld", function(req, res){
-    res.send("<h1>Hello World! Server is functioning properly!</h1>")
-});
-*/
-
 app.use(routes);
-
-app.get("/api/articles", function(req, res){
-    Article.find({}, function(err, data){
-        if (err){
-            console.log(err);
-        }
-        console.log("We hit this route");
-        res.json(data);
-    });
-});
-
-
-//Catch-all route that is meant to serve the express app
-app.get("*", function(req, res){
-    res.sendFile(path.join(__dirname, "/nytreact-client/build/index.html"));
-});
+app.use(express.static("nytreact-client/build")); //If no other routes are hit, serve the react client
 
 app.listen(PORT, function() {
     console.log("App now listening at localhost:" + PORT);
